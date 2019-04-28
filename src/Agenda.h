d1ddb297c659192d441030b0d79b2ec98ad426ea
Bernardo Santos <bernardosantos670@gmail.com>
2019-04-28 05:11:39 +0100

    Added Agenda

---
+++ src/Agenda.h
@@ -0,0 +1,32 @@
+#ifndef AGENDA_H
+#define AGENDA_H
+
+
+#include <vector>
+#include <string>
+
+#include "Coordinates.h"
+#include "Time.h"
+
+
+class Activity;
+
+class Agenda {
+
+private:
+    std::vector<Activity *> activities
+};
+
+class Activity {
+
+private:
+    std::string info;
+    Coordinates coords;
+    Time startTime, duration;
+
+public:
+
+};
+
+
+#endif
