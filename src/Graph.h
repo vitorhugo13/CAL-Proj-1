87b246866648843aff0d193e25e8f486f62c5fe5
unknown <vhlg99@gmail.com>
2019-05-02 15:28:58 +0100

    Needed to complement dijkstra algorithm

--- src/Graph.h
+++ src/Graph.h
@@ -24,6 +24,8 @@ public:
 
     void dijsktraAlgorithm(const std::string &src, const std::string &dest){
 
+
+//dist é o time
         for(unsigned int i=0;i<vertexSet.size();i++){
 		    vertexSet[i]->path=NULL;    //o que esta parte do algoritmo faz é percorrer o vertexSet todo
 		    vertexSet[i]->time=TIME_LIMIT;		//mete o path a null, a distancia como infinito e visited a false
@@ -41,13 +43,23 @@ public:
 	    q.insert(startVertex);
 
         while(!q.empty()){
-            //need to be completed
+            Vertex* v=q.extractMin();
+
+            for(size_t i=0;i<v->paths.size();i++){
+			    Edge edge=v->paths[i];
+			    Vertex* w=v->paths[i]->time;
+                
+                if(edge.getTravelTime< w->time){
+                        w->time=edge.getTravelTime;
+                        w->path=v;
+                }
+		    }
         }
 	}
 
 	
     
-    //quando encontrar o vertex de destino paramos
+    //quando for encontrado o vertex de destino o algoritmo não pode percorrer mais.
 
 };
 
