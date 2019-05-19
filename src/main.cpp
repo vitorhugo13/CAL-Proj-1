fbf822633aafef5eb236956a1e00e5a1f73836bf
Vitor Hugo <vhlg99@gmail.com>
2019-05-19 16:30:55 +0100

    added things at main

--- src/main.cpp
+++ src/main.cpp
@@ -61,7 +61,9 @@ int main(void) {
 
 	int menu = -2;
 	while(menu != 4){
+
 		while(menu == -2){
+
 			std::cout << "----- TripMate ----" << std::endl<< std::endl;
 			std::cout << "0 - See Agenda" << std::endl;
 			std::cout << "1 - Add activity" << std::endl;
@@ -70,19 +72,28 @@ int main(void) {
 			std::cout << "4 - Exit " << std::endl;
 
 			menu = menuInput(4);
+
 			if(menu == -1)
 				std::cout << "Menu does not exist" << std::endl;
+
 			switch (menu){
 					case 0:{
 						agenda.show(seeAgenda());
 						menu = -2;
 						break;
 						}
+						
 					case 1:{
-						agenda.addActivity();
+						if(agenda.addActivity()){
+							std::cout<< "Activity added with success!"<< std::endl;
+						}
+						else{
+							std::cout<< "Activity can not be added!"<< std::endl;
+						}
 						menu = -2;
 						break;
 					}
+
 					case 2:{
 						std::string name, date;
 						std::cout << "Activity's name ?" << std::endl;
@@ -93,18 +104,21 @@ int main(void) {
 						if(agenda.removeActivity(name, date))
 							std::cout << "Activity removed with success! " << std::endl;
 						else
-							std::cout << "Activity can not be added" << std::endl;
+							std::cout << "Activity can not be removed!" << std::endl;
 						menu = -2;
 						break;
 					}
+
 					case 3:{
 						agenda.show(seeAgenda());
 						std::cout << "From which activity we want to see the path to the next activity ? " << std::endl;
 						menu = -2;
 						break;
 					}
+
 					case 4:
 						return 0;
+						
 					default:
 						return 0;
 				}
