5c49f1d4f5d3a8caaf5db925c0dd9e10ac789254
Vitor Hugo <vhlg99@gmail.com>
2019-05-21 17:53:26 +0100

    deleted not important stuff

--- src/Graph.h
+++ src/Graph.h
@@ -62,59 +62,6 @@ public:
 	vector<T> getPath(const T &origin, const T &dest) const;
 
 };
-/*
 
-
-
-
-    
-
-
-
-    //dist é o time
-    //Implementação do algoritmo de dijsktra
-
-    void dijsktraAlgorithm(const std::string &src, const std::string &dest){
-
-
-
-        for(unsigned int i=0;i<vertexSet.size();i++){
-		    vertexSet[i]->path=NULL;    //o que esta parte do algoritmo faz é percorrer o vertexSet todo
-		    vertexSet[i]->time=TIME_LIMIT;		//mete o path a null, a distancia como infinito e visited a false
-		    vertexSet[i]->visited=false;// assim todos os pontos ficam em igualdade
-	    }
-
-	    Vertex* startVertex=this->findVertex(src); 
-        if(!startVertex){
-		    return;
-        }
-        startVertex->time=0;
-
-	    MutablePriorityQueue<Vertex> q;
-	    startVertex->visited=true;
-	    q.insert(startVertex);
-
-        while(!q.empty()){
-            Vertex* v=q.extractMin();
-
-            for(size_t i=0;i<v->paths.size();i++){
-			    Edge edge=v->paths[i];
-			    Vertex* w=v->paths[i]->time;
-                
-                if(edge.getTravelTime< w->time){
-                        w->time=edge.getTravelTime;
-                        w->path=v;
-                }
-		    }
-        }
-	}
-
-	
-    
-    //quando for encontrado o vertex de destino o algoritmo não pode percorrer mais.
-
-};
-
-*/
 
 #endif
